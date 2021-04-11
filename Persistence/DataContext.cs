863338b7500008bbf8ab2c29736e3f69f56eb147
Intermundo Seguridad <feg92@hotmail.com>
2021-04-11 18:12:43 -0300

    End of session 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Activity> Activities { get; set; }
+
+        public DataContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+
+
+    }
+}
