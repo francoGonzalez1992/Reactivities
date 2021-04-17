ac8ddab2864eefdf88738a28c54d5646c91b10e8
Intermundo Seguridad <feg92@hotmail.com>
2021-04-17 14:54:51 -0300

    End of Section 3

---
+++ client-app/src/DuckItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Duck } from './demo'
+
+interface Props {
+    duck: Duck;
+
+}
+
+export default function DuckItem({duck}: Props) {
+    return (
+        <div>
+            <div>
+                <span>{duck.name}</span><br />
+                <button onClick={() => duck.makeSound(duck.name + ' quack')}>Make  sound</button>
+            </div>
+        </div>
+    )
+}
