807214c6dc7e6ba41a450bc7aafa50da3c02ce80
franco <info.fran.92@gmail.com>
2021-05-16 23:44:47 -0300

    end of Section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import './app/layout/style.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter } from 'react-router-dom';
-
+import 'react-calendar/dist/Calendar.css'
+import './app/layout/style.css';
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
